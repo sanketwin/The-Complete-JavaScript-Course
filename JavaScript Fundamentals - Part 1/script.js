b922ec6d9e93dbff2a7be8663c6c1bd7406178f5
sanketwin <sanketwin@gmail.com>
2025-02-04 23:30:12 +0530

    Day 1 Commit

---
+++ JavaScript Fundamentals - Part 1/script.js
@@ -0,0 +1,116 @@
+// Values and Variables -----
+
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Sanket");
+console.log(21);
+
+let firstName = "Sanket";
+console.log(firstName);
+
+// Variable name conventions
+let sanket_rasal = "SR";
+let $function = 21;
+
+let person = "Sanket";
+let PI = 3.1415;
+
+let myFirstJob = "Full Stack Developer";
+let myCurrentJob = "Angular Developer";
+
+console.log(myFirstJob);
+
+
+// Data Types -----
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Sanket");
+
+
+javascriptIsFun = "YES!"
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1998;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+// let, const and var
+
+
+let age = 21;
+age = 22;
+
+
+const birthYear = 1998;
+// birthYear = 1999;
+
+// const job;
+
+var job = "programmer";
+job = "coder";
+
+lastName = "Rasal"
+console.log(lastName);
+
+
+
+// Operators
+
+const now = 2025;
+const ageSanket = now - 1998;
+const ageDurga = now - 2004
+console.log(ageSanket, ageDurga);
+
+
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+console.log(ageSanket * 2, ageSanket / 10, 2 ** 3);
+
+const firstName = "Sanket";
+const lastName = "Rasal";
+
+console.log(firstName + lastName);
+console.log(firstName + " " +lastName);
+
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+
+// Comparison Operator
+console.log(ageSanket > ageDurga); // >, <, >=, <=
+
+console.log(ageDurga <= 18);
+
+const isFullAge = ageDurga >= 18;
+
+console.log(now - 1998 > now - 2004);
+
+
+*/
+
+// Operator Precedence
+
+const now = 2025;
+const ageSanket = now - 1998;
+const ageDurga = now - 2004
+
+console.log(now - 1998 > now - 2004);
