13fb98c42debc22b5bbf2e4e06f4c60f8dc52c64
SRasalParkar <srasal@parkar.digital>
2025-05-12 14:01:48 +0530

    function clousers & challenge 2

--- 05 - A Closer Look At Functions/script.js
+++ 05 - A Closer Look At Functions/script.js
@@ -189,7 +189,83 @@ const addTaxRate = (rate) => {
   };
 };
 
-const addVAT2 = addTaxRate(0.23);
+// const addVAT2 = addTaxRate(0.23);
 
-console.log(addVAT2(100));
-console.log(addVAT2(120));
+// console.log(addVAT2(100));
+// console.log(addVAT2(120));
+
+//SECTION Immediately Invoked Function Expressions (IIFE)
+
+// Normal function
+// const runOnce = function () {
+//   console.log('This will never run again');
+// };
+
+// runOnce();
+
+// (function () {
+//   console.log('This will never run again - Using function keyword');
+//   const isPrivate = 23;
+// })();
+
+// (() => console.log('This will never run again - Using arrow function'))();
+
+//SECTION Closures
+
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 77;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+//Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
