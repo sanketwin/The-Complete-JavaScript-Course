0aca6d3b3a7e88fc4854c3d36d56c12ce002cc11
SRasalParkar <srasal@parkar.digital>
2025-05-12 15:48:09 +0530

    Initial commit

---
+++ 06 - Working With Arrays [PROJECT]/script.js
@@ -0,0 +1,14 @@
+'use strict';
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
